48066e5b3149631ce3464253a31425d099dc2d22
clara-tyman2 <clara.tyman@stu.ocsb.ca>
2025-03-03 10:02:20 -0500

    main.c file

---
+++ mian.c
@@ -0,0 +1,47 @@
+/*
+ Created by Clara T
+
+ Created on Feb 2025
+
+
+ Turns LED on for one second, the off for one second, repetedly, changing colours
+*/
+
+void setup()
+{
+    pinMode(11, OUTPUT); // Green
+    pinMode(12, OUTPUT); // Blue
+    pinMode(13, OUTPUT); // Red
+}
+
+void loop()
+{
+    digitalWrite(11, HIGH);
+    digitalWrite(12, LOW);
+    digitalWrite(13, LOW);
+    delay(1000);
+    digitalWrite(11, LOW);
+    digitalWrite(12, HIGH);
+    digitalWrite(13, LOW);
+    delay(1000);
+    digitalWrite(11, LOW);
+    digitalWrite(12, LOW);
+    digitalWrite(13, HIGH);
+    delay(1000);
+    digitalWrite(11, HIGH);
+    digitalWrite(12, LOW);
+    digitalWrite(13, HIGH);
+    delay(1000);
+    digitalWrite(11, HIGH);
+    digitalWrite(12, HIGH);
+    digitalWrite(13, LOW);
+    delay(1000);
+    digitalWrite(11, LOW);
+    digitalWrite(12, HIGH);
+    digitalWrite(13, HIGH);
+    delay(1000);
+    digitalWrite(11, HIGH);
+    digitalWrite(12, HIGH);
+    digitalWrite(13, HIGH);
+    delay(1000);
+}
